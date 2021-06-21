accc4778838d6dbc3b804d27cf57ee65fb804845
Adam Call <amcall21@gmail.com>
2021-06-21 01:16:48 -0500

    create multiple linear regression

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,13 @@
 # Read in the MechaCar_mpg.csv
-mpg_data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+mpg_data <- read.csv(file='Resources\\MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
+# add tidyverse library
+library(tidyverse)
+
+#Perform linear regression on the mpg_data
+lm(vehicle_length~vehicle_weight+spoiler_angle+
+     ground_clearance+AWD+mpg, data = mpg_data)
+
+#summary of linear regression.
+summary(lm(vehicle_length~vehicle_weight+spoiler_angle+
+             ground_clearance+AWD+mpg, data = mpg_data))
